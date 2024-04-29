d107108fe6f85e09137dc390977fe2165ecd39ba
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-04-29 23:09:09 +0200

    Update models.js

--- models.js
+++ models.js
@@ -24,6 +24,15 @@ let userSchema = mongoose.Schema({
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
+// hashing of submitted passwords
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+// compares submitted hashed passwords with the hashed passwords of database
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
