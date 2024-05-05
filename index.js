35618fa684b320ec6f2f55def1c1cc975fc63584
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-05-05 18:05:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,11 @@ const Users = Models.User; // Model name defined in models.js
 
 // CONLINE DATABASE - allows Mongoose to connect to db (to perform CRUD operations on the containing documents)
 mongoose.connect(
-  'mongodb+srv://tobiaskraft1:tk1234@tobflixdb.vkqp8sh.mongodb.net/tobflixDB?retryWrites=true&w=majority&appName=tobflixDB',
+  mongoose.connect('mongodb+srv://tk:<password>@kraftflixdb.sfx6qkl.mongodb.net/?retryWrites=true&w=majority&appName=kraftFlixDB', {
+  	useNewUrlParser: true,
+ 	useUnifiedTopology: true,
+});
+,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
